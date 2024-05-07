07ab77b5dfaee66be745e4058055355bd4a920f7
Vitória Beltrão <vbwdoo@gmail.com>
2024-05-07 11:21:08 -0300

    Adicionando no XCode

---
+++ Estudando Calculadora/Estudando Calculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Estudando Calculadora
+//
+//  Created by Vitória Beltrão Wenceslau do Ó on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Estudando Calculadora/Estudando Calculadora/Estudando_CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estudando_CalculadoraApp.swift
+//  Estudando Calculadora
+//
+//  Created by Vitória Beltrão Wenceslau do Ó on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Estudando_CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
